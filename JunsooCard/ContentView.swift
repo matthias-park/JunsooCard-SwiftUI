4463eadcd4e3b53d7c631cc966ac0c06195a851d
Junsoo Park <matthiaspark0514@gmail.com>
2021-12-14 23:09:16 -0800

    add Fonts and SwiftUIs features in my business card app

--- JunsooCard/ContentView.swift
+++ JunsooCard/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red: 0.37, green: 0.15, blue: 0.80, opacity: 1.00)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                Image("Junsoo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Junsoo Park")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+01 123 456 7890", imageName: "phone.fill")
+                InfoView(text: "junsoopark@email.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
@@ -19,3 +42,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
