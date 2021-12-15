853fd35c4a184cdb905f759deb1dce250a22ad7d
Junsoo Park <matthiaspark0514@gmail.com>
2021-12-14 18:53:52 -0800

    Initial Commit

---
+++ JunsooCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JunsooCard
+//
+//  Created by JUNSOO PARK on 2021-12-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JunsooCard/JunsooCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JunsooCardApp.swift
+//  JunsooCard
+//
+//  Created by JUNSOO PARK on 2021-12-14.
+//
+
+import SwiftUI
+
+@main
+struct JunsooCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
